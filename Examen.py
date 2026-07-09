106fe48d1f374e197c6acf7c320fd017f3c26f73
jucarvajalg-jpg <ju.carvajalg@duocuc.cl>
2026-07-09 19:48:06 -0400

    Primer Commit

---
+++ Examen.py
@@ -0,0 +1,4 @@
+# =====================================================================
+# ASIGNATURA: Fundamentos de Programación (FPY1101)
+# EVALUACIÓN: Evaluación Final Transversal (EFT) - PetMarket
+# =====================================================================
